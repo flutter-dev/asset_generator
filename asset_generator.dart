6cec8de1c4128c5c3b24101e69ffca6e85dd0d89
debuggerx01 <dx8917312@163.com>
2020-09-01 11:02:18 +0800

    Update asset_generator.dart

--- asset_generator.dart
+++ asset_generator.dart
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Copyright 2018 DebuggerX <dx8917312@gmail.com>. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'dart:io';
 
 var preview_server_port = 2227;
